5b1535112f9ae8acee73e84b45688d37371b6922
Javi Ortega <rotegras@gmail.com>
2021-04-18 14:03:32 +0200

    fix - styles

--- src/components/AddDeck.js
+++ src/components/AddDeck.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {
   TextInput,
   StyleSheet,
+  View,
 } from 'react-native';
 import { connect } from 'react-redux';
 import { addDeck } from '../redux/actions';
@@ -19,35 +20,46 @@ function AddDeck({dispatch, navigation}) {
   }
 
   return (
-    <Card style={styles.card}>
-      <Card.Title title='Enter the name of the new deck' />
-      <TextInput
-        value={deckName}
-        placeHolder='Enter Deck Name'
-        onChangeText={setDeckName}
-        style={styles.input}
-        />
-      <Button
-        onPress={handleAddDeck}
-        disabled={deckName === ''}
-        mode='contained'
-        icon="pencil-plus"
-      >
-        Submit
-      </Button>
-    </Card>
+    <View style={styles.container}>
+      <Card style={styles.card}>
+        <Card.Title title='Add new Deck' />
+        <TextInput
+          value={deckName}
+          placeholder='Enter Deck Name'
+          onChangeText={setDeckName}
+          style={styles.input}
+          />
+        <Button
+          onPress={handleAddDeck}
+          disabled={deckName === ''}
+          mode='contained'
+          icon="pencil-plus"
+        >
+          Submit
+        </Button>
+      </Card>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  },
   card: {
     marginTop: 16,
     marginBottom: 16,
-
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 10,
   },
   input: {
     height: 40,
     margin: 12,
+    backgroundColor: '#ebebeb',
+    paddingRight: 10,
+    paddingLeft: 10,
   },
 });
 
