d7679f7ac26552e37ea17406174dea90ec8a7b03
Javi Ortega <rotegras@gmail.com>
2021-04-20 14:51:51 +0200

    refactor - delete old files

--- src/components/Decks.js
+++ src/components/Decks.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import * as S from './DecksCollection.style';
 import { Avatar, Card, Title } from 'react-native-paper';
 import {
   ScrollView,
