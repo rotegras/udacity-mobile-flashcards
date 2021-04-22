5eeb1ef0b657ef1a3896b208d6f873576723ca6b
Javi Ortega <rotegras@gmail.com>
2021-04-22 15:49:35 +0200

    feature - convert scrollview to flatlist

--- src/components/Decks.js
+++ src/components/Decks.js
@@ -2,49 +2,69 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Avatar, Card, Title } from 'react-native-paper';
 import {
-  ScrollView,
+  FlatList,
   StyleSheet,
   View,
+  Dimensions,
 } from 'react-native';
 
-// TODO: LAZY LOAD ITEMS
-function Decks({ decks, navigation }) {
+function CardItem({ deck, navigation }) {
   return (
-    <ScrollView
-      style={[styles.container, {flex: 1}]}>
-      { decks
-        ? decks.map((deck) => (
-          <Card
-            key={deck.name}
-            onPress={() => navigation.navigate('Deck Details', { itemId: deck.name })}
-            style={styles.card}
-          >
-          <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: 12, }}>
-            <Avatar.Text
-              size={48}
-              label={deck.name.split('')[0].toUpperCase()}
-            />
-            <Card.Title
-              title={deck.name}
-              subtitle={`${deck.questions.length} cards`}
-            />
-          </View>
-        </ Card>
-        ))
-        : <View>There are no decks to show</View>
-      }
-      </ScrollView>
+    <Card
+      key={deck.name}
+      onPress={() => navigation.navigate('Deck Details', { itemId: deck.name })}
+      style={styles.card}
+    >
+      <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: 12, }}>
+        <Avatar.Text
+          size={48}
+          label={deck.name.split('')[0].toUpperCase()}
+        />
+        <Card.Title
+          title={deck.name}
+          subtitle={`${deck.questions.length} cards`}
+        />
+      </View>
+    </ Card>
+  )
+}
+
+function Decks({ decks, navigation }) {
+  const renderItem= ({item}) => (
+    <CardItem
+      deck={item}
+      navigation={navigation}
+    />
+  )
+
+  return (
+    <View>
+    { decks
+      ? <FlatList
+          style={styles.container}
+          data = {decks}
+          keyExtractor={(item) => item.name}
+          renderItem={renderItem}
+        />
+      : <View>There are no decks to show</View>
+    }
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: Dimensions.get('window').width,
+    minHeight: Dimensions.get('window').height,
+    marginBottom: 100,
   },
   card: {
     marginTop: 10,
     marginLeft: 10,
     marginRight: 10,
+    height: 80,
+    flex: 1,
   }
 });
 
