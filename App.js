d45abc2bef8702fb33fd932b3dfeddf7a68403f1
Javi Ortega <rotegras@gmail.com>
2021-05-07 12:20:33 +0200

    feature - add local notifications

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform , View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store, persistor } from './src/redux/store/store';
 import MainTabNavigation from './src/components/MainTabNavigation';
 import { PersistGate } from 'redux-persist/integration/react';
-
+import { setLocalNotification } from './src/utils/helpers';
 
 export default function App() {
+
+  useEffect(() => {
+    setLocalNotification();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,8 @@
 import { decks, quiz } from './data';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 function _getInitialData() {
   return Promise.all([
@@ -36,4 +40,71 @@ function timeToString (time = Date.now()) {
 // saveDeckTitle: take in a single title argument and add it to the decks.
 // addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
 
-export { _getAllDecks, _getAllQuiz, _getInitialData, timeToString };
+
+const NOTIFICATION_KEY = 'MobileFlashCards:notifications'
+
+function getDailyReminderValue () {
+  return {
+    today: "👋 Don't forget to study today!"
+  }
+}
+
+function clearLocalNotification () {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync);
+}
+
+function createNotification () {
+  return {
+    title: 'It is time to study',
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true,
+    }
+  }
+}
+
+function setLocalNotification () {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync();
+
+              let tomorrow = new Date();
+              tomorrow.setDate(tomorrow.getDate() + 1);
+              tomorrow.setHours(22);
+              tomorrow.setMinutes(45);
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              );
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          })
+          .catch((error) => console.log(error));
+      }
+    })
+}
+
+
+export {
+  _getAllDecks,
+  _getAllQuiz,
+  _getInitialData,
+  timeToString,
+  getDailyReminderValue,
+  clearLocalNotification,
+  createNotification,
+  setLocalNotification,
+};
